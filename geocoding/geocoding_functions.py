4b157a27d2b9de81904cd87f9fe4ff05c1a895d1
Emanuele Bugliarello <emanuele.bugliarello@epfl.ch>
2017-01-06 11:42:50 +0100

    Update geocoding

--- geocoding/geocoding_functions.py
+++ geocoding/geocoding_functions.py
@@ -1,10 +1,26 @@
 from pygeocoder import Geocoder as geo
 
-# returns True if the input address is valid, False otherwise
-def isAddressValid(address):
-	return geo.geocode(address).valid_address
+def is_address_valid(address):
+    """
+    Check if the passed address is valid.
+    :param address: string representing a street address. E.g., "Avenue du Temple 3, Renens, Switzerland"
+    :return: True if the address is valid, False otherwise
+    """
+    result = False
+    try:
+        result = geo.geocode(address).valid_address
+    except:
+        pass
+    return result
 
-# returns a tuple (latitude, longtitude) corresponding to coordinates of a valid address
-# undefined behavior if address is not valid
-def getCoordinates(address):
-	return geo.geocode(address).coordinates
+
+def get_coordinates(address):
+    """
+    Retrieve coordinates of given street address.
+    :param address: string representing a street address. E.g., "Avenue du Temple 3, Renens, Switzerland"
+    :return: a tuple (latitude, longtitude) corresponding to coordinates of a valid address, None otherwise
+    """
+    if is_address_valid(address):
+        return geo.geocode(address).coordinates
+    else:
+        return None
--- geocoding/location_functions.py
+++ geocoding/location_functions.py
@@ -1,24 +1,58 @@
-import math
+import numpy as np
 
-# returns distance between two points
-def getDistance(point1, point2):
-	return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+def haversine_distance(x, y):
+    """
+    Compute the haversine distance between two points.
+    :param x: point specified as (latitude,  longitude)
+    :param y: point specified as (latitude,  longitude)
+    :return: haversine distance between x and y in meters
+    """
+    R = 6371000
+    theta1 = x[0] * np.pi / 180
+    theta2 = y[0] * np.pi / 180
+    delta_theta = (y[0] - x[0]) * np.pi / 180
+    delta_lambda = (y[1] - x[1]) * np.pi / 180
+    a = np.sin(delta_theta / 2) ** 2 + np.cos(theta1) * np.cos(theta2) * np.sin(delta_lambda / 2) ** 2
+    c = 2 * np.arctan(np.sqrt(a) / np.sqrt(1 - a))
+    distance = R * c
+
+    return distance
+
+
+def get_distance(point1, point2):
+    """
+    Compute haversine distance between point1 and point2.
+    :param point1: location point specified as (latitude, longitude)
+    :param point2: location point specified as (latitude, longitude)
+    :return: haversine distance in meters
+    """
+    assert (point1 is not None),"point1 has invalid coordinates"
+    assert (point2 is not None),"point2 has invalid coordinates"
+    return haversine_distance(point1, point2)
+
 
 # returns a tuple (distance, stationID)
-def getNearestStation(house, stations):
-	nearest = None
-	distance = None
-	for idx in range(0, len(stations)):
-		current_distance = getDistance(house, stations[idx])
-		if(distance is None or current_distance < distance):
-			distance = current_distance
-			nearest = idx
-	return (distance, nearest)
+def get_nearest_station(house, stations):
+    """
+    Retrieve nearest station to house.
+    :param house: (lat, lon) coordinates of house address
+    :param stations: list of TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+    :return: 
+    """
+    nearest = None
+    distance = None
+    for idx in range(0, len(stations)):
+        current_distance = get_distance(house, stations[idx])
+        if(distance is None or current_distance < distance):
+            distance = current_distance
+            nearest = idx
+    return (distance, nearest)
 
 # returns a sorted list of (distance, stationID) tuples
-def getStationDistances(house, stations):
-	distances = []
-	for idx in range(0, len(stations)):
-		current_distance = getDistance(house, stations[idx])
-		distances.append((current_distance, idx))
-	return sorted(distances)
+def get_station_distances(house, stations):
+    distances = []
+    for idx in range(0, len(stations)):
+        current_distance = get_distance(house, stations[idx])
+        distances.append((current_distance, idx))
+    return sorted(distances)
